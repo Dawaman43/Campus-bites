ec1f0ab633a2bab55cbee11306a1ec992ab5596c
Chala751 <ctemesgen85@gmail.com>
2025-04-14 18:13:49 -0300

    made the home page responsive

--- components/Food.tsx
+++ components/Food.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { View, Text, Image, ImageSourcePropType, StyleSheet } from 'react-native';
+import { View, Text, Image, ImageSourcePropType, StyleSheet,Dimensions } from 'react-native';
 
 interface CardProps {
   imageSource: ImageSourcePropType;
   text: string;
   fee: number;
 }
-
+const {width} = Dimensions.get('window')
 const Card: React.FC<CardProps> = ({ imageSource, text, fee }) => {
   return (
-    <View className="m-1 p-2 rounded-lg">
+    <View className="mr-5 rounded-lg">
       <Image source={imageSource} style={styles.image} />
       <Text className="mt-4 text-base text-gray-700">{text}</Text>
       <Text className="text-xl font-bold text-gray-800">{fee} Birr</Text>
@@ -19,7 +19,7 @@ const Card: React.FC<CardProps> = ({ imageSource, text, fee }) => {
 
 const styles = StyleSheet.create({
   image: {
-    width: 100,  // equivalent to w-28
+    width: width*0.40,  // equivalent to w-28
     height: 100, // equivalent to h-28
     borderRadius: 8, // equivalent to rounded-lg
   },
